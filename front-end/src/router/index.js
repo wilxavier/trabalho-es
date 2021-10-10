4cf6085b861eb20ac485cba5ff5968ef586ba595
mjsil <m.j.silveira@outlook.com>
2021-10-10 19:24:22 -0300

    Rotas da aplicacao

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { HomePage } from '../pages/Home';
+import { DashboardPage } from '../pages/Dashboard';
+import { SignInPage } from '../pages/SignIn';
+import { EditPlayPage } from '../pages/EditPlay';
+import { BuyTicketPage } from '../pages/BuyTicket';
 import { NavBar } from '../components/NavBar';
 import { Footer } from '../components/Footer';
 
@@ -10,7 +14,11 @@ export const Routes = () => {
     <Router>
       <NavBar />
       <Switch>
-        <Route patg='/' exact component={HomePage} />
+        <Route path={'/'} exact component={HomePage} />
+        <Route path={'/dashboard'} component={DashboardPage} />
+        <Route path={'/signin'} component={SignInPage} />
+        <Route path={'/editplay'} component={EditPlayPage} />
+        <Route path={'/buyticket'} component={BuyTicketPage} />
       </Switch>
       <Footer />
     </Router>
