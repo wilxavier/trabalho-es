dbaec5b01550009258604c31fbfd498178ca2a42
mjsil <m.j.silveira@outlook.com>
2021-10-09 23:58:04 -0300

    estrutura inicial do projeto

---
+++ front-end/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Routes } from './router';
+import { GlobalStyle } from './global';
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Routes />
+    </>
+  );
+};
+
+export default App;
