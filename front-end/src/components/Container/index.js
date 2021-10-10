98540fc87beb2ede116baf0137a407f7c3148bd1
mjsil <m.j.silveira@outlook.com>
2021-10-10 00:00:54 -0300

    Componente global Container

---
+++ front-end/src/components/Container/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  width: 100%;
+  max-width: 120rem;
+  margin: 0 auto;
+  padding: 0 3.125rem;
+
+  @media screen and (max-width: 991px) {
+    padding: 0 1.875rem;
+  }
+`;
