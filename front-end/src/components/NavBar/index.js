0374b84e890e3b9861d35216d9d09a3d35f5a248
mjsil <m.j.silveira@outlook.com>
2021-10-11 08:32:33 -0300

    Personalizando componente NavBar

--- front-end/src/components/NavBar/index.js
+++ front-end/src/components/NavBar/index.js
@@ -12,7 +12,7 @@ export const NavBar = () => {
 
         <Menu>
           <MenuItem>
-            <MenuLink to={'/dashboard'}>Login / Cadastro</MenuLink>
+            <MenuLink to={'/signin'}>Login / Cadastro</MenuLink>
           </MenuItem>
         </Menu>
       </Wrapper>
