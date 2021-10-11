82f610caaf0f14635ff4b9924ac20146c1c950dd
mjsil <m.j.silveira@outlook.com>
2021-10-11 08:33:05 -0300

    Adicionando novas rotas na aplicacao

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { HomePage } from '../pages/Home';
 import { DashboardPage } from '../pages/Dashboard';
 import { SignInPage } from '../pages/SignIn';
+import { SignUpPage } from '../pages/SignUp';
+import { ProfilePage } from '../pages/Profile';
 import { EditPlayPage } from '../pages/EditPlay';
 import { BuyTicketPage } from '../pages/BuyTicket';
 import { NavBar } from '../components/NavBar';
@@ -17,6 +19,8 @@ export const Routes = () => {
         <Route path={'/'} exact component={HomePage} />
         <Route path={'/dashboard'} component={DashboardPage} />
         <Route path={'/signin'} component={SignInPage} />
+        <Route path={'/signup'} component={SignUpPage} />
+        <Route path={'/profile'} component={ProfilePage} />
         <Route path={'/editplay'} component={EditPlayPage} />
         <Route path={'/buyticket'} component={BuyTicketPage} />
       </Switch>
